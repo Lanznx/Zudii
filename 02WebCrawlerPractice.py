57a7ce255c487538c3c56400ff03dd869e2d2950
Brandon <109305092@g.nccu.edu.tw>
2022-01-23 16:29:11 +0800

    試試看用 VScode

---
+++ 02WebCrawlerPractice.py
@@ -0,0 +1,24 @@
+# Action Chains
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+path = "/Users/lanz/Downloads/檔案/chromedriver" ## webdriver 的路徑
+driver = webdriver.Chrome(path)
+driver.get("https://tsj.tw/") ##遊戲網站
+
+
+blow = driver.find_element(By.ID, "click") ##開始吹
+# blow_count = driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[4]/div[2]/h4[2]')
+
+actions = ActionChains(driver)
+for u in range(1000): ##一直吹
+    actions.click(blow)
+    actions.double_click(blow)
+    actions.perform()
+    
+
+
+
+
